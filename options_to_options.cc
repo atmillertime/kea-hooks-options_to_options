dfadfebbf02f093bae8053639d1ff14030430a93
Arnoud Vermeer <avermeer@paxio.com>
2019-02-28 11:03:16 +0100

    Missing include statement

--- options_to_options.cc
+++ options_to_options.cc
@@ -1,3 +1,5 @@
+#include <options_to_options.h>
+
 namespace options_to_options {
     std::string toText(const std::vector<uint8_t>& binary) {
         return std::string(binary.begin(), binary.end());
